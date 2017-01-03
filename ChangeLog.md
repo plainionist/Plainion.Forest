d2d42fc79a7d611c53066885a30f0c5f45907c43
ronin4net <pinguin751@gmx.net>
2017-01-03 17:32:54 +0100

    migrated to Plainion.CI

---
+++ ChangeLog.md
@@ -0,0 +1,7 @@
+## 1.1.0 - 2017-01-03
+
+- minor cleanups
+
+## 1.0.0 - 2015-05-02
+
+- Initial release
--- build/GlobalAssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-using System.Reflection;
-
-[assembly: AssemblyDescription( "Plainion.Forest" )]
-[assembly: AssemblyCopyright( "Copyright © 2015" )]
-[assembly: AssemblyVersion( "1.1.0.0" )]
-[assembly: AssemblyProduct( "Plainion.Forest" )]
